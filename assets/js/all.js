c96fb529502580bbda31aea94aa64f26359c200e
Yikai <shuffle3105@gmail.com>
2022-09-13 21:49:37 +0800

    Update 2022-09-13T13:49:16.462Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,39 +1,36 @@
+"use strict";
+
 $(document).ready(function () {
-  $('.navbar-toggler').click(function () { 
+  $('.navbar-toggler').click(function () {
     $('body').toggleClass('navbar-nav_show');
     $('.menu').toggleClass('d-none');
     $('.close').toggleClass('d-block');
   });
-
-  $('.btn-search').click(function () { 
+  $('.btn-search').click(function () {
     $('.searchBar').removeClass('d-none');
     $(this).parent().parent().find('h1').addClass('d-none');
     $(this).addClass('d-none');
     $('.navbar-toggler').addClass('d-none');
   });
-
-  $('.btn-back').click(function () { 
+  $('.btn-back').click(function () {
     $('.searchBar').addClass('d-none');
     $(this).parent().parent().parent().find('h1').removeClass('d-none');
     $('.btn-search').removeClass('d-none');
     $('.navbar-toggler').removeClass('d-none');
   });
-
-  $('.btn-ranking').click(function () { 
+  $('.btn-ranking').click(function () {
     $(this).toggleClass('open');
     $(this).find('.plus').toggleClass('d-none');
     $(this).find('.minus').toggleClass('d-block');
   });
-
-  $('.link-wallet-item').click(function () { 
+  $('.link-wallet-item').click(function () {
     $(this).toggleClass('chose');
     $(this).find('.fa-check').toggleClass('d-block');
     $(this).siblings().removeClass('chose');
     $(this).siblings().find('.fa-check').removeClass('d-block');
   });
-
-  $('.row').imagesLoaded().progress( function() {
+  $('.row').imagesLoaded().progress(function () {
     $('.row').masonry();
   });
-
 });
+//# sourceMappingURL=all.js.map
